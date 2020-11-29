4d0d1966111df1c984856a4881963d76ccb80892
Tulio Molina <tuliojose8@gmail.com>
2020-11-29 16:14:46 -0400

    refactored code to save db storage

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,63 +1,6 @@
 import axios from "axios";
 import mongoClient from "./mongoclient";
 
-export const updateResource = async (
-  endpointBaseUrl: string
-): Promise<any[]> => {
-  // fetching total number of resources
-  const totalResources = (
-    await axios.post(
-      endpointBaseUrl,
-      {},
-      {
-        params: { size: 0 },
-      }
-    )
-  ).data.total;
-
-  // setting counters
-  let offset = 0;
-  let size = 5000;
-  let remaining = totalResources;
-
-  const promiseContainer = [];
-
-  // fetching all resources available at torre, request 5000 resources at a time (size param limit).
-  // all requests are triggered in parallel for better network performance
-  while (remaining > 0) {
-    const resourcePromise = axios.post(
-      endpointBaseUrl,
-      {},
-      {
-        params: { size, offset },
-      }
-    );
-    // pushing network request promise to array of promises to fetch in parallel
-    promiseContainer.push(resourcePromise);
-
-    offset += size;
-    remaining -= size;
-    if (remaining < size) {
-      size = remaining;
-    }
-
-    console.log(remaining);
-  }
-
-  const responseArray = await Promise.all(promiseContainer);
-  console.log("after promise all");
-
-  let buffer: any = [];
-
-  responseArray.forEach((responseElement) => {
-    buffer.push(responseElement.data.results);
-  });
-
-  const resultingResources = buffer.flat();
-
-  return resultingResources;
-};
-
 export const updateResourceDb = async (
   endpointBaseUrl: string,
   resource: string
@@ -91,11 +34,17 @@ export const updateResourceDb = async (
         }
       );
 
+      const resourceBatch = resources.data.results;
+
+      // delete skills property to save db storage
+      if (resource === "people") {
+        resourceBatch.forEach((person: any) => {
+          delete person.skills;
+        });
+      }
+
       // inserting resources to db
-      await mongoClient
-        .db()
-        .collection(resource)
-        .insert(resources.data.results);
+      await mongoClient.db().collection(resource).insert(resourceBatch);
 
       // updating counters
       offset += size;
