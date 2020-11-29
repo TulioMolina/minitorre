b702996bc3ffe1fda472e650e86fc7a92d61497e
Tulio Molina <tuliojose8@gmail.com>
2020-11-28 23:09:31 -0400

    update-data endpoints implemented

---
+++ backend/src/helpers.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+export const updateResource = async (
+  endpointBaseUrl: string
+): Promise<any[]> => {
+  // fetching total number of resources
+  const totalResources = (
+    await axios.post(
+      endpointBaseUrl,
+      {},
+      {
+        params: { size: 0 },
+      }
+    )
+  ).data.total;
+
+  // setting counters
+  let offset = 0;
+  let size = 5000;
+  let remaining = totalResources;
+
+  const promiseContainer = [];
+
+  // fetching all resources available at torre, request 5000 resources at a time (size param limit).
+  // all requests are triggered in parallel for better network performance
+  while (remaining > 0) {
+    const resourcePromise = axios.post(
+      endpointBaseUrl,
+      {},
+      {
+        params: { size, offset },
+      }
+    );
+    // pushing network request promise to array of promises to fetch in parallel
+    promiseContainer.push(resourcePromise);
+
+    offset += size;
+    remaining -= size;
+    if (remaining < size) {
+      size = remaining;
+    }
+
+    console.log(remaining);
+  }
+
+  const responseArray = await Promise.all(promiseContainer);
+  console.log("after promise all");
+
+  const resultingResources: any[] = [];
+
+  responseArray.forEach((responseElement) => {
+    resultingResources.concat(responseElement.data.results);
+  });
+
+  return resultingResources;
+};
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,9 +1,50 @@
 import express, { Request, Response } from "express";
+// import axios from "axios";
+
+import { updateResource } from "./helpers";
 
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  res.send("hello");
-});
+let peopleData: any[];
+let opportunitiesData: any[];
+
+router.get(
+  "/",
+  async (req: Request, res: Response): Promise<void> => {
+    res.send("hello");
+  }
+);
+
+// endpoint that updates and stores in-memory opportunities data. To be used every certain amount of minutes
+router.get(
+  "/update/opportunities",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      // defining base url for people endpoint at torre
+      const opportBaseUrl = `https://search.torre.co/opportunities/_search/`;
+      opportunitiesData = await updateResource(opportBaseUrl);
+      res.send(`len ${opportunitiesData.length}`);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("error");
+    }
+  }
+);
+
+// endpoint that updates and stores in-memory people data. To be used every certain amount of minutes
+router.get(
+  "/update/people",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      // defining base url for people endpoint at torre
+      const peopleBaseUrl = `https://search.torre.co/people/_search/`;
+      peopleData = await updateResource(peopleBaseUrl);
+      res.send(`len ${peopleData.length}`);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("error");
+    }
+  }
+);
 
 export default router;
